39c17b5cae2f8a9377336263cd03c817c1b7e734
Mahasak Pijittum <mahasak@gmail.com>
2019-03-30 23:53:25 +0800

    test gcloud deploy

--- functions/index.js
+++ functions/index.js
@@ -5,5 +5,5 @@ const functions = require('firebase-functions');
 //
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase-Changed!");
+  response.send("Hello from Firebase-Gcloud!");
 });
